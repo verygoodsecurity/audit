1498531754f516dfa577ecb2d09ad282e0bfed0c
Kent Rosenkoetter <kent.rosenkoetter@vgs.io>
2020-11-25 12:12:57 -0800

    Major version bump.

--- src/main/java/io/vgs/track/meta/NotTracked.java
+++ src/main/java/io/vgs/track/meta/NotTracked.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks an {@link javax.persistence.Entity} field as omitted from tracking.  This overrides a
+ * {@link Tracked} annotation applied at the class level.  It is an error for a field to be annotated
+ * with both {@link Tracked} and {@link NotTracked} simultaneously.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface NotTracked {
--- src/main/java/io/vgs/track/meta/Trackable.java
+++ src/main/java/io/vgs/track/meta/Trackable.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks an {@link javax.persistence.Entity} as trackable.  Any fields within the entity that are
+ * annotated with {@link Tracked} are tracked.  If the entity itself is marked as {@link Tracked},
+ * then all fields not annotated with {@link NotTracked} are tracked.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Trackable {
